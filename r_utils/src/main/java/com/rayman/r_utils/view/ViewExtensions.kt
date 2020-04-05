f6932a7b6524b15b56d03fb609dfb65cc3537d3b
Rayman <lvrayman@gmail.com>
2020-04-05 23:24:32 +0800

    改变isVisible名称防止与官方库冲突

--- r_utils/src/main/java/com/rayman/r_utils/view/ViewExtensions.kt
+++ r_utils/src/main/java/com/rayman/r_utils/view/ViewExtensions.kt
@@ -11,10 +11,10 @@ import android.view.inputmethod.InputMethodManager
  * @author 吕少锐 (lvrayman@gmail.com)
  * @version 2019-08-17
  */
-inline var View.isVisible: Boolean
+inline var View.visible: Boolean
     get() = visibility == View.VISIBLE
     set(value) {
-        if (isVisible != value) {
+        if (visible != value) {
             visibility = if (value) View.VISIBLE else View.GONE
         }
     }
